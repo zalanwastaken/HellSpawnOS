81bba37b01ca0e236f601425008346edc7a78d65
zalanwastaken <muditmishra.mr@gmail.com>
2026-01-16 11:48:03 +0530

    small change to test workflow

--- src/os/kernel/kernel.c
+++ src/os/kernel/kernel.c
@@ -23,9 +23,10 @@ void kernel_main(void){
 
     for (int i = 0; i<100; i++){
         for(int f = 0; f<100; f++){
-            vbe_putpixel(graphicsInfo, i, f, vbe_rgb(graphicsInfo, 255, 255, 255));
+            vbe_putpixel(graphicsInfo, i, f, vbe_rgb(graphicsInfo, 255, 0, 255));
         }
     }
+
     while (1){
         asm volatile("hlt"); // just STOP
     }
