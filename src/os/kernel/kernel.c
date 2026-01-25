0646f54cdf95a5df02346900977e8e565612edda
zalanwastaken <muditmishra.mr@gmail.com>
2026-01-25 11:42:09 +0530

    test update

--- src/os/kernel/kernel.c
+++ src/os/kernel/kernel.c
@@ -9,8 +9,9 @@
 void init(){
     IDT_init();
     PIC_init();
-    asm volatile("sti");
     mem_manager_init();
+
+    asm volatile("sti");
 }
 
 __attribute__((section(".start")))
@@ -23,7 +24,7 @@ void kernel_main(void){
 
     init();
 
-    for (int i = 0; i<150; i++){
+    for (int i = 0; i<220; i++){
         for(int f = 0; f<100; f++){
             vbe_putpixel(graphicsInfo, i, f, vbe_rgb(graphicsInfo, 255, 255, 255));
         }
@@ -31,9 +32,14 @@ void kernel_main(void){
     vbe_draw_string_scaled(0, 0, "Hello World !", vbe_rgb(graphicsInfo, 255, 0, 0), 2);
 
     //TEST
-
-    int *mem = (int*)kalloc(515);
-    kfree(mem);
+    uint16_t mem = (uint16_t)kalloc(512);
+    serial_print_hex(mem);
+    serial_print("\n");
+    kfree((void*)mem);
+    serial_print_hex((uint32_t)kalloc(512));
+    serial_print("\n");
+    serial_print_hex((uint32_t)kalloc(512));
+    serial_print("\n");
 
     while (1){
         asm volatile("hlt");
