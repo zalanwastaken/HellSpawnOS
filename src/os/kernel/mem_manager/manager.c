063bb25a28f965fd22a72d6e9b9bdea8ea18dff0
zalanwastaken <muditmishra.mr@gmail.com>
2026-01-25 11:41:35 +0530

    bug fixes and rewrite

--- src/os/kernel/mem_manager/manager.c
+++ src/os/kernel/mem_manager/manager.c
@@ -2,7 +2,6 @@
 #include"../serial/serial.h"
 
 manager_Data *data = (manager_Data*)0x800;
-size_t *kernel_size = (size_t*)0x7E0F;
 
 int ceil(double x) {
     int i = (int)x;
@@ -16,47 +15,56 @@ int ceil(double x) {
 }
 
 void mem_manager_init(){
+    size_t *kernel_size = (size_t*)0x7E0F;
     data->block_size = 512;
     data->startfrom = 0x1000+kernel_size[0]+0x200; //? start AFTER the kernel, leave 0x200 for data n stuff
     data->allocatedBlocks_idx = 0;
 }
 
 void* kalloc(size_t sizeToAlloc){
-    size_t allocsize = ceil((double)sizeToAlloc/data->block_size); // blocks to alloc
-    void* ret;
-    for(uint32_t i = 0; i < allocsize; i++){
-        data->allocatedBlock[data->allocatedBlocks_idx] = data->startfrom+(data->allocatedBlocks_idx*data->block_size);
-        if(i == 0){
-            ret = (void*)data->allocatedBlock[data->allocatedBlocks_idx];
+    size_t need = ceil((double)sizeToAlloc / data->block_size);
+    uint32_t startblock = data->startfrom;
+    while (1){
+        bool ok = true;
+        // check if we have "need" free blocks from startblock
+        for(size_t b = 0; b < need; b++){
+            uint32_t addr = startblock + b * data->block_size;
+            for(uint32_t i = 0; i < data->allocatedBlocks_idx; i++){
+                if(data->allocatedBlock[i] == addr){
+                    ok = false;
+                    break;
+                }
+            }
+            if(!ok){
+                break;
+            };
         }
-        data->allocatedBlocks_idx++;
+        if(ok){
+            // reserve blocks
+            for(size_t b = 0; b < need; b++){
+                data->allocatedBlock[data->allocatedBlocks_idx++] =
+                    startblock + b * data->block_size;
+            }
+            return (void*)startblock;
+        }
+        // try next block
+        startblock += data->block_size;
     }
-    data->allocatedBlock[data->allocatedBlocks_idx] = 0x0000; //? we start after the kernel so data here can never be 0x0, so we use these as seperators
-    data->allocatedBlocks_idx++;
-    return ret;
 }
 
 void kfree(void *toFree){
-    uint32_t start = 0xFFFFFFFF;
-    uint32_t end = 0;
-    for(uint32_t i = 0; i < data->allocatedBlocks_idx; i++){
-        if((void*)data->allocatedBlock[i] == toFree){
-            start = i;
-            break;
+    uint32_t addr = (uint32_t)toFree;
+
+    for(uint32_t i = 0; i < data->allocatedBlocks_idx; ){
+        if(data->allocatedBlock[i] == addr){
+            // shift left to delete entry
+            for(uint32_t j = i; j < data->allocatedBlocks_idx - 1; j++){
+                data->allocatedBlock[j] = data->allocatedBlock[j + 1];
+            }
+            data->allocatedBlocks_idx--;
+            addr += data->block_size; // next block of same alloc
+        } else {
+            i++; // only advance if not deleting
         }
     }
-    if(start == 0xFFFFFFFF) return; // not found
-    // find separator (end)
-    for(uint32_t i = start; i < data->allocatedBlocks_idx; i++){
-        if(data->allocatedBlock[i] == 0x0000){
-            end = i;
-            break;
-        }
-    }
-    // shift everything left
-    uint32_t shift = end - start + 1;
-    for(uint32_t i = start; i + shift < data->allocatedBlocks_idx; i++){
-        data->allocatedBlock[i] = data->allocatedBlock[i + shift];
-    }
-    data->allocatedBlocks_idx -= shift;
 }
